31de4a23db0233706cb501a9948b107626f93c0e
parikshit-restack <frontend@restacking.co>
2024-12-15 13:33:32 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 import { config } from './wagmi-config'
-import { writeContract, switchChain } from '@wagmi/core'
+import { writeContract, switchChain, simulateContract, waitForTransactionReceipt } from '@wagmi/core'
 import { erc20abi } from './abi'
 import { useAccount, useConnect, WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -29,12 +29,25 @@ function AppContent() {
         functionName: 'approve',
         args: ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 1n],
       })
+      // simulate contract 
+      const {request} = await simulateContract(config, {
+        address: "0x4d191d317b24Af004FaEE9db7371f5d4C5F5aC53",
+        abi: erc20abi,
+        functionName: 'transferFrom',
+        args: ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 1n],
+      })
+      
       const result  = await writeContract(config, {
         address: "0x4d191d317b24Af004FaEE9db7371f5d4C5F5aC53",
         abi: erc20abi,
         functionName: 'transferFrom',
         args: ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 1n],
       })
+      
+      // const result  = await writeContract(config, request)
+
+      const result2 = await waitForTransactionReceipt(result.hash)
+
       console.log(approveHash)
     } catch (error) {
       console.error("Contract interaction failed:", error)
